3355c3fc1d4c867b88588054eefe120aa9ad15b8
Miguel Antonio <migant1306@gmail.com>
2024-05-08 19:16:21 -0300

    DTO cliente e novos pacotes

--- src/v1/client/dto/client.dto.ts
+++ src/v1/client/dto/client.dto.ts
@@ -1,70 +1,106 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateClientBody {
   @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   nome: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   cpf: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   data_nascimento: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   cep: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   n_casa: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   bairro: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  logradouro: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   municipio: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   telefone: number;
 }
 
 export class FindByEmailBody {
   @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
 
 export class UpdateClientBody {
   @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   nome: string;
 
   @ApiProperty()
+  image_url: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   cpf: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   data_nascimento: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   cep: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   n_casa: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   bairro: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  logradouro: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   municipio: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   telefone: number;
 }
